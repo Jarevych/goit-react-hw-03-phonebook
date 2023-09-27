bc51e6fa1969c5d0f9ebd68bb525074e0d793786
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-09-27 21:15:27 +0300

    adding LS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,22 @@ import ContactList from './ContactList';
 import FormInput from './Form';
 import './styles.css';
 
+
 export class App extends React.Component {
   state = {
     contacts: [],
     filter: '',
   };
+
   addContact = newContact => {
+    const { name, number } = newContact;
     const contactExists = this.state.contacts.some(
       contact =>
-        contact.name.toLowerCase().includes(newContact.name.toLowerCase()) ||
-        contact.number === newContact.number
+        contact.name.toLowerCase().includes(name.toLowerCase()) ||
+        contact.number === number
     );
     if (contactExists) {
-      alert(`"${newContact.name}"is already in contacts`);
+      alert(`"${name}"is already in contacts`);
       return;
     }
     this.setState(prevState => ({
@@ -35,6 +38,20 @@ export class App extends React.Component {
       contacts: prevState.contacts.filter(contact => contact.id !== itemId),
     }));
   };
+
+  componentDidMount() {
+    const stringifiedContacts = localStorage.getItem('contacts');
+    const parcedContacts = JSON.parse(stringifiedContacts) ?? [];
+    this.setState({
+      contacts: parcedContacts,
+    });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts.length !== prevState.contacts.length) {
+      const stringifiedContacts = JSON.stringify(this.state.contacts);
+      localStorage.setItem('contacts', stringifiedContacts);
+    }
+  }
 
   render() {
     const filteredContacts = this.state.contacts.filter(contact =>
